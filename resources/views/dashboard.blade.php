39a6839f308744d69f125f940bc574908a713357
Nasreen <nasreenaqmal12@gmail.com>
2026-07-21 20:48:52 +0800

    feat: secure user roles and add admin provisioning

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout title="Dashboard">
+
+
+    <script type="module" src="{{ Vite::asset('resources/js/dashboard.js') }}"></script>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [SessionController::class, 'create'])->name('login');
     Route::post('/login', [SessionController::class, 'store']);
     Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
     Route::post('/register', [RegisteredUserController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
 });
